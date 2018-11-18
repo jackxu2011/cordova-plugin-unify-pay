a05a186dc33a20d68a1f11955f28c33298181165
Jack Xu <xulingcom@126.com>
2018-11-18 08:39:27 +0800

    1.add permission judge,2.fix the qmf jar str '==' opt 的天坑

--- src/android/Unifypay.java
+++ src/android/Unifypay.java
@@ -1,5 +1,8 @@
 package com.linkcld.cordova;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
@@ -7,6 +10,9 @@ import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 
 import com.chinaums.pppay.unify.UnifyPayListener;
 import com.chinaums.pppay.unify.UnifyPayPlugin;
@@ -26,7 +32,19 @@ public class Unifypay extends CordovaPlugin implements UnifyPayListener {
     public static final String ERROR_WECHAT_RESPONSE_UNSUPPORT = "微信不支持";
     public static final String ERROR_WECHAT_RESPONSE_UNKNOWN = "未知错误";
 
+    public static final String ERROR_PERMISSION_DENIED_ERROR = "权限请求被拒绝";
+
+    /**
+     * 安卓6以上动态权限相关
+     */
+    private static final int REQUEST_CODE = 1100001;
+    
+    public static final String[] permissions = { Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+
     protected static CallbackContext currentCallbackContext;
+
+    private CountDownLatch countDownLatch;
 
     @Override
     protected void pluginInitialize() {
@@ -38,32 +56,108 @@ public class Unifypay extends CordovaPlugin implements UnifyPayListener {
         Log.d(TAG, "plugin initialized.");
     }
 
+    public static boolean isAlipayInstalled(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            return packageManager.getPackageInfo("com.eg.android.AlipayGphone", 0) != null;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean hasPermissions() {
+        for(String p : permissions) {
+            if(!cordova.hasPermission(p)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+        ArrayList<String> permissionsToRequire = new ArrayList<String>();
+
+        for(String p : permissions) {
+            if(!cordova.hasPermission(p)) {
+                permissionsToRequire.add(p);
+            }
+        }
+
+        String[] _permissionsToRequire = new String[permissionsToRequire.size()];
+        _permissionsToRequire = permissionsToRequire.toArray(_permissionsToRequire);
+        cordova.requestPermissions(this, REQUEST_CODE, _permissionsToRequire);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != REQUEST_CODE) {
+            return;
+        }
+        countDownLatch.countDown();
+    }
+
     @Override
-    public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) {
         Log.i(TAG, "Execute:" + action + " with :" + args.toString());
         
         if(action.equals("pay")) {
-            String channel = args.getString(0);
-            String payData = args.getString(1);
-            return sendPaymentRequest(channel, payData, callbackContext);
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    if(!hasPermissions()) {
+                        countDownLatch = new CountDownLatch(1);
+                        requestPermission();
+                        try {
+                            countDownLatch.await();
+                            if(!hasPermissions()) {
+                                Log.e(TAG, ERROR_PERMISSION_DENIED_ERROR);
+                                getCurrentCallbackContext().error(ERROR_PERMISSION_DENIED_ERROR);
+                                return;
+                            }
+                        } catch (InterruptedException e) {
+                            Log.e(TAG, e.getMessage());
+                            getCurrentCallbackContext().error(e.getMessage());
+                        }
+                    }
+                    try {
+                        String channel = args.getString(0);
+                        String payData = args.getString(1);
+//                        if(!isAlipayInstalled(cordova.getActivity())) {
+//                            callbackContext.error("未安装支付宝");
+//                            return;
+//                        }
+                        sendPaymentRequest(channel, payData, callbackContext);
+                    } catch (JSONException e) {
+                        Log.i(TAG, e.getMessage());
+                        callbackContext.error("参数错误");
+                    }
+                }
+            });
+            return true;
         }
         return false;
     }
 
-    protected boolean sendPaymentRequest(String channel, String payData, CallbackContext callbackContext) {
-        try {
-            currentCallbackContext = callbackContext;
+    protected void sendPaymentRequest(String channel, String payData, CallbackContext callbackContext) {
+        currentCallbackContext = callbackContext;
+        channel = getChannel(channel);
+        if(channel != null)  {
             UnifyPayRequest msg = new UnifyPayRequest();
-            msg.payChannel = channel;
+            msg.payChannel = UnifyPayRequest.CHANNEL_ALIPAY;
             msg.payData = payData;
             UnifyPayPlugin.getInstance(cordova.getActivity()).sendPayRequest(msg);
             sendNoResultPluginResult(callbackContext);
-            return true;
-        } catch (Exception e) {
-            Log.i(TAG, e.getMessage());
+        } else {
             callbackContext.error("参数错误");
         }
-        return true;
+    }
+
+    private String getChannel(String channel) {
+        switch (channel) {
+            case UnifyPayRequest.CHANNEL_ALIPAY: return UnifyPayRequest.CHANNEL_ALIPAY;
+            case UnifyPayRequest.CHANNEL_WEIXIN: return UnifyPayRequest.CHANNEL_WEIXIN;
+            case UnifyPayRequest.CHANNEL_UMSPAY: return UnifyPayRequest.CHANNEL_UMSPAY;
+            default: return null;
+        }
     }
 
     @Override
